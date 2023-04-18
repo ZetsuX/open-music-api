0b33e9a186784f6e0959bc3ce4f11746a06ebb07
Fehlling Halim <kevnhalim@gmail.com>
2023-04-18 16:47:13 +0700

    feat: add albums validator

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -2,10 +2,10 @@ const AlbumsHandler = require("./handler");
 const routes = require("./routes");
 
 module.exports = {
-    name: "notes",
+    name: "albums",
     version: "1.0.0",
     register: async (server, { service, validator }) => {
-        const notesHandler = new AlbumsHandler(service, validator);
-        server.route(routes(notesHandler));
+        const albumsHandler = new AlbumsHandler(service, validator);
+        server.route(routes(albumsHandler));
     },
 };
