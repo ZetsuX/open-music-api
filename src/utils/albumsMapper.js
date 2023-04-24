a66e085709b30131952283e0c824c911a5f47cb1
Fehlling Halim <kevnhalim@gmail.com>
2023-04-24 23:02:47 +0700

    feat: change album map for cover_url

--- src/utils/albumsMapper.js
+++ src/utils/albumsMapper.js
@@ -1,8 +1,16 @@
 /* eslint-disable camelcase */
-const albumMapDBToModel = ({ id, name, year, created_at, updated_at }) => ({
+const albumMapDBToModel = ({
     id,
     name,
     year,
+    cover_url,
+    created_at,
+    updated_at,
+}) => ({
+    id,
+    name,
+    year,
+    coverUrl: cover_url,
     createdAt: created_at,
     updatedAt: updated_at,
 });
