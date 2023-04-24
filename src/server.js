08eebd3f99df3ca4b6819adb4ea7859a60c1c66c
Fehlling Halim <kevnhalim@gmail.com>
2023-04-24 20:13:31 +0700

    feat: add playlist export producer with message broker

--- src/server.js
+++ src/server.js
@@ -28,6 +28,10 @@ const collaborations = require("./api/collaborations");
 const CollaborationsService = require("./services/postgres/CollaborationsService");
 const CollaborationsValidator = require("./validator/collaborations");
 
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
@@ -122,6 +126,15 @@ const init = async () => {
                 playlistsService,
                 usersService,
                 validator: CollaborationsValidator,
+            },
+        },
+
+        {
+            plugin: _exports,
+            options: {
+                producerService: ProducerService,
+                playlistsService,
+                validator: ExportsValidator,
             },
         },
     ]);
